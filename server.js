33246077666ab22fbff7cb29a42a36fce27262ad
B L Λ C K <ixd.abhi@gmail.com>
2020-10-09 17:48:59 -0400

    Fixed the lost file for EEG connection

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
+    socket = require('socket.io'),
     app = express(),
-    server = app.listen(3000);
+    server = app.listen(3000, () => {
+        console.log("NUOS Registration running on 3000...");
+    }),
+    io = socket(server);
 
-require('./app/neurosky')(server);
+require('./app/neurosky')(io);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
